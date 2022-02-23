17dff4dc936d9e20dadd574156e51032133d9ca9
Adrian Torchiana <adrian.torchiana@gmail.com>
2022-02-23 15:06:58 -0800

    Save maps, add a bit more context in readme

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # regression_with_spatial_correlation
 
+What happens when you fit a linear regression to spatially correlated data,
+and calculate std errors assuming the data are i.i.d.?
+
+In this simulation, the researcher's goal is to produce unbiased estimates and valid
+confidence intervals for beta_hat in a setting where Y = constant + x*beta + epsilon.
+
+Related:
+ - https://voxeu.org/article/standard-errors-persistence
+ - https://papers.ssrn.com/sol3/papers.cfm?abstract_id=3398303
+
 ![Sampling Distribution](sampling_distribution_beta_x1_with_varying_levels_of_spatial_correlation.png)
 
 ![CI Coverage](ci_coverage_for_beta_x1_with_varying_levels_of_spatial_correlation.png)
--- simulation.R
+++ simulation.R
@@ -5,14 +5,45 @@ library(data.table)
 library(ggplot2)
 library(MASS)
 
-run_single_simulation <- function(locations, covariance_x1, covariance_epsilon, beta_x1) {
+save_maps_single_simulation <- function(locations, distances, length_x1, length_epsilon, beta_x1) {
 
+    covariance_x1 <- gaussian_kernel(distances, length=length_x1)
+    covariance_epsilon <- gaussian_kernel(distances, length=length_epsilon)
+
+    locations <- simulate_spatial_data(locations, covariance_x1, covariance_epsilon, beta_x1)
+
+    p <- (ggplot(locations, aes(x=coord_x, y=coord_y, fill=epsilon)) +
+          geom_raster() +
+          scale_fill_gradient2("epsilon", low="#ef8a62", mid="white", high="#67a9cf", midpoint = 0) +
+          xlab("pixel coordinate (easting)") +
+          ylab("pixel coordinate (northing)") +
+          theme_bw())
+    filename <- "map_epsilon_single_simulation.png"  # TODO Put length epsilon in filename
+    ggsave(filename=filename, plot=p, width=8, height=6, unit="in")
+
+    p <- (ggplot(locations, aes(x=coord_x, y=coord_y, fill=y)) +
+          geom_raster() +
+          scale_fill_gradient2(low="#ef8a62", mid="white", high="#67a9cf", midpoint = 0) +
+          xlab("pixel coordinate (easting)") +
+          ylab("pixel coordinate (northing)") +
+          theme_bw())
+    filename <- "map_y_single_simulation.png"
+    ggsave(filename=filename, plot=p, width=8, height=6, unit="in")
+}
+
+simulate_spatial_data <- function(locations, covariance_x1, covariance_epsilon, beta_x1) {
     locations$x1 <- mvrnorm(n=1, mu=rep(0, nrow(locations)), Sigma=covariance_x1)
     locations$epsilon <- mvrnorm(n=1, mu=rep(0, nrow(locations)), Sigma=covariance_epsilon)
 
     ## True regression function: y = X*beta + epsilon
     ## TODO Include an x2 in the true regression function, just to make it interesting?
     locations$y <- 10 + beta_x1*locations$x1 + locations$epsilon
+    return(locations)
+}
+
+simulate_spatial_data_and_fit_model <- function(locations, covariance_x1, covariance_epsilon, beta_x1) {
+
+    locations <- simulate_spatial_data(locations, covariance_x1, covariance_epsilon, beta_x1)
 
     model <- lm(y ~ x1, data=locations)
     summary(model)
@@ -32,7 +63,7 @@ run_simulations <- function(locations, distances, length_x1, length_epsilon, bet
     ## Variance-covariance matrix must be positive (semi-) definite
     all(eigen(covariance_epsilon)$epsilons > 0)
 
-    simulations <- as.data.frame(t(replicate(n_sims, run_single_simulation(locations, covariance_x1, covariance_epsilon, beta_x1))))
+    simulations <- as.data.frame(t(replicate(n_sims, simulate_spatial_data_and_fit_model(locations, covariance_x1, covariance_epsilon, beta_x1))))
     simulations$length_epsilon <- length_epsilon
 
     return(simulations)
@@ -45,6 +76,8 @@ gaussian_kernel <- function(distance, sigma=2, length=5) {
 ## Quick and dirty spatial locations (points in a grid)
 grid_width <- 20
 locations <- expand.grid(coord_x=seq(1, grid_width), coord_y=seq(1, grid_width))
+
+## Sample size for regressions is grid_width^2 = 400 points
 dim(locations)
 head(locations)
 
@@ -55,7 +88,8 @@ distances[1, 22]
 
 beta_x1 <- 5
 
-## TODO Run a single simulation and plot spatial data run_single_simulation
+## Run a single simulation and visualize it
+save_maps_single_simulation(locations, distances, length_x1=2, length_epsilon=2, beta_x1=beta_x1)
 
 lengths_epsilon <- c(1/8, 0.25, 0.5, 1, 2, 4, 6, 8, 10)
 
@@ -90,11 +124,10 @@ p <- (ggplot(coverage, aes(x=length_epsilon, y=pr_conf_interval_contains_beta_x1
       geom_hline(yintercept=0.95, linetype=2, alpha=0.5) +
       ylab("coverage of 95% confidence interval for beta") +
       xlab("kernel length for epsilon (controls spatial correlation)") +
+      scale_y_continuous(breaks=seq(0, 1, 0.2), limits=c(0, 1)) +
       theme_bw())
 filename <- "ci_coverage_for_beta_x1_with_varying_levels_of_spatial_correlation.png"
 ggsave(filename=filename, plot=p, width=8, height=6, unit="in")
-
-## TODO Run for a range of kernel lengths for epsilon and plot coverage probability
 
 ## TODO Test for spatial autocorrelation in errors
 
@@ -104,19 +137,3 @@ ggsave(filename=filename, plot=p, width=8, height=6, unit="in")
 
 ## TODO Plot semi variogram (both of Y and of residuals!)
 ## What matters (for messing up CI coverage) is spatial correlation in residuals, not in X
-
-## p <- (ggplot(locations, aes(x=coord_x, y=coord_y, fill=epsilon)) +
-##       geom_raster() +
-##       scale_fill_gradient2("epsilon", low="#ef8a62", mid="white", high="#67a9cf", midpoint = 0) +
-##       xlab("pixel coordinate (easting)") +
-##       ylab("pixel coordinate (northing)") +
-##       theme_bw())
-## p
-
-## p <- (ggplot(locations, aes(x=coord_x, y=coord_y, fill=y)) +
-##       geom_raster() +
-##       scale_fill_gradient2(low="#ef8a62", mid="white", high="#67a9cf", midpoint = 0) +
-##       xlab("pixel coordinate (easting)") +
-##       ylab("pixel coordinate (northing)") +
-##       theme_bw())
-## p
